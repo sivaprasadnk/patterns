8615b24f0a873fb4af1743d6af355834aebaec1d
sivaprasadnk <sivaprasadnk123@gmail.com>
2025-01-31 16:20:20 +0530

    adding right angle triangle

---
+++ lib/pattern1.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+/*
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+*/
+
+void main() {
+  int n = 5;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write('* ');
+    }
+    print('');
+  }
+}
